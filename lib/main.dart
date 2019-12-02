296f2ef31aeadc5ab0625a16b67bb0794e22b966
Chris Sells <csells@sellsbrothers.com>
2019-12-01 22:28:18 -0800

    point at generated static content hosted as github pages; add support for atom pagination.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// TODO: hook Dart networking such that an HTTP GET for a relative url, e.g. /foo/bar.jpg,
+// can be patched to be absolute, e.g. https://csells.github.io/sb6/foo/bar.jpg
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -27,7 +30,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    loadAtomFeed();
+    loadAtomFeed('https://csells.github.io/sb6/feed.atom');
   }
 
   @override
@@ -35,24 +38,56 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(title: Text('sellsbrothers.com')),
         body: feed == null
             ? Center(child: CircularProgressIndicator())
-            : ListView(
-                children: feed.items
-                    .map(
-                      (item) => ListTile(
-                        title: Text(item.title),
-                        subtitle: Text(item.summary),
-                        trailing: Image.network(item.links.firstWhere((l) => l.rel == 'enclosure', orElse: () => AtomLink('http://localhost:8080/public/favicon.ico', null, null, null, null, null))?.href),
-                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage(item))),
-                      ),
-                    )
-                    .toList(),
+            : Column(
+                children: [
+                  Expanded(
+                    child: ListView(
+                      children: feed.items
+                          .map(
+                            (item) => ListTile(
+                              title: Text(item.title),
+                              subtitle: Text(item.summary),
+                              trailing: Image.network(item.links.firstWhere((l) => l.rel == 'enclosure', orElse: () => AtomLink('https://csells.github.io/sb6/public/favicon.ico', null, null, null, null, null))?.href),
+                              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage(item))),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.spaceEvenly,
+                    layoutBehavior: ButtonBarLayoutBehavior.padded,
+                    children: [
+                      RaisedButton(child: Text('<<'), onPressed: getPressedHandler('first')),
+                      RaisedButton(child: Text('<'), onPressed: getPressedHandler('previous')),
+                      RaisedButton(child: Text('>'), onPressed: getPressedHandler('next')),
+                      RaisedButton(child: Text('>>'), onPressed: getPressedHandler('last')),
+                    ],
+                  ),
+                ],
               ),
       );
 
-  void loadAtomFeed() async {
-    var resp = await http.Client().get('http://localhost:8080/feed.atom');
-    var xmlString = resp.body;
-    setState(() => feed = AtomFeed.parse(xmlString));
+  void loadAtomFeed(String feedlink) async {
+    var resp = await http.Client().get(feedlink);
+    setState(() => feed = AtomFeed.parse(resp.body));
+  }
+
+  // This function doesn't handle the button presses; it created the appropriate handler that will be called when the button is pressed.
+  // We want the button to be disenabled if there's nothing to do, e.g. no previous page or we're on the first/last page
+  // In those cases, we return null. This causes the button to be disabled.
+  // In the other case, when pressing the button can do something, we return the function that will be called when the button is pressed.
+  // This is a little tricky even for me...
+  Function getPressedHandler(String rel) {
+    if (feed == null) return null;
+
+    var relatedFeed = feed.links.firstWhere((l) => l.rel == rel, orElse: () => null);
+    if (relatedFeed == null) return null;
+
+    var selfFeed = feed.links.firstWhere((l) => l.rel == 'self', orElse: () => null);
+    if (relatedFeed.href == selfFeed.href) return null;
+
+    return () => loadAtomFeed(relatedFeed.href);
   }
 }
 
